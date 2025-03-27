33786fba4da4a1d528fe28f5fbec06bc1c05fd06
azizhammi <116007181+azizhammi@users.noreply.github.com>
2025-03-27 04:13:18 +0100

    Update OCELObject.java

--- src/org/processmining/newpackageivy/plugins/OCELdata/OCELObject.java
+++ src/org/processmining/newpackageivy/plugins/OCELdata/OCELObject.java
@@ -18,45 +18,38 @@ public class OCELObject {
     public List<OCELEvent> sortedRelatedEvents;
     public Map<String, String> relatedObjectIdentifiers;
     
-    public Map<String, Object> attributes; // The first value of the object attribute
+    public Map<String, Object> attributes; 
     
-    public Map<String, Map<Date, Object>> timedAttributes; // The evolution of the object attribute values
-
+    public Map<String, Map<Date, Object>> timedAttributes; 
     public OCELObject(OCELEventLog eventLog) {
         this.eventLog = eventLog;
-        this.relatedEvents = new HashSet<OCELEvent>(); // Avoid duplicates in events
+        this.relatedEvents = new HashSet<OCELEvent>();
         this.attributes = new HashMap<String, Object>();
         this.sortedRelatedEvents = new ArrayList<OCELEvent>();
         this.relatedObjectIdentifiers = new HashMap<String, String>();
         this.timedAttributes = new HashMap<String, Map<Date, Object>>();
     }
 
-    // Add the object to its objectType
     public void register() {
         this.objectType.objects.add(this);
         this.sortEvents();
     }
 
-    // Sort the related events
     public void sortEvents() {
         this.sortedRelatedEvents = new ArrayList<OCELEvent>(relatedEvents);
         Collections.sort(this.sortedRelatedEvents, new OCELEventComparator());
     }
 
-    // Method to get attribute keys (for filter)
     public Set<String> getAttributeKeys() {
         return attributes.keySet();
     }
     
     
-    
-    //test
     public OCELObject(String id) {
         this.id = id;
         this.timedAttributes = new HashMap<>();
     }
 
-    // Method to set timed attributes
     public void setTimedAttributes(Map<String, Map<Date, Object>> timedAttributes) {
         this.timedAttributes = timedAttributes;
     }
