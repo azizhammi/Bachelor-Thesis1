84ee7a5f0c6ef9f28e6c7e1510665a1a2d9412c5
azizhammi <116007181+azizhammi@users.noreply.github.com>
2025-03-27 04:14:07 +0100

    Update OCELObjectType.java

--- src/org/processmining/newpackageivy/plugins/OCELdata/OCELObjectType.java
+++ src/org/processmining/newpackageivy/plugins/OCELdata/OCELObjectType.java
@@ -12,22 +12,21 @@ import java.util.HashSet;
 public class OCELObjectType {
 
 	
-	public OCELEventLog eventLog; // The event log to which this object type belongs.
-    public String name; // Name of the object type
-    public Set<OCELObject> objects; // the objects of this type in the event log.
+    public OCELEventLog eventLog;
+    public String name;
+    public Set<OCELObject> objects;
     
-    public Map<String, Object> attributes; // Stores attributes for this object type
+    public Map<String, Object> attributes; 
 
 
    
     
     public OCELObjectType(OCELEventLog eventLog, String name) {
         this.eventLog = eventLog;
-        this.name = name; // the type name.
-        this.objects = new HashSet<OCELObject>(); // Initializes the  objects for this type.
+        this.name = name; 
+        this.objects = new HashSet<OCELObject>(); 
         
-        this.attributes = new HashMap<>(); // Initializes the attributes map.
-
+        this.attributes = new HashMap<>(); 
         
     }
     
